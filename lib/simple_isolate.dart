b1698a2d3daf630833d4ff11607aee15da0f0e0c
Даниил Азевич <dazevichweb@yandex.ru>
2022-05-21 11:28:13 +0200

    FIX
    - убран лишний код

--- lib/simple_isolate.dart
+++ lib/simple_isolate.dart
@@ -1,28 +1,5 @@
-import 'dart:convert';
+library simple_isolates;
 
-import 'package:simple_isolate/src/classes/post.dart';
-import 'package:simple_isolate/src/classes/request.dart';
-import 'package:simple_isolate/src/simple_isolate.dart';
-import 'package:http/http.dart' as http;
-
-void main() async {
-  final runner = await SimpleIsolates.getRunner();
-
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
-
-  final request = RequestSync<List<Post>>(
-      arguments: {'response' : response},
-      callBack: (arguments){
-        final response = arguments['response'] as http.Response;
-        final comments = json.decode(utf8.decode(response.bodyBytes));
-        final result = (comments as List<dynamic>).map((post){
-          return Post.fromJson(post);
-        });
-        return result.toList();
-      }
-  );
-  final result = await runner.run(request);
-  print('get ${result?.length}');
-
-  runner.close();
-}
+export 'src/simple_isolate.dart';
+export 'src/classes/request.dart';
+export 'src/classes/answer.dart';
